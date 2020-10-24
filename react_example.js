09cd240a4113bb11815a7d8d0f6a9a3524ee954a
an1m3sh7 <57225231+an1m3sh7@users.noreply.github.com>
2020-10-24 12:52:43 +0530

    Create react_example.js
    reactJS example

---
+++ react_example.js
@@ -0,0 +1,19 @@
+class Cat extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      humor: 'happy'
+    }
+  }
+  render() {
+    return(
+      <div>
+        <h1>{this.props.name}</h1>
+        <p>
+          {this.props.color}
+        </p>
+      </div>
+    );
+  }
+}
